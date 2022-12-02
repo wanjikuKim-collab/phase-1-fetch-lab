eb27e5b980923a0161ecf5b78d36aa96541adf56
Wanjiku Kimani <wanjikukimani24@gmail.com>
2022-12-02 14:46:23 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(res => res.json())
+    .then(json=> renderBooks(json));
 }
 
 function renderBooks(books) {
@@ -13,5 +15,6 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  return fetchBooks();
 });
+
